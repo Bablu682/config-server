e6cdfeaa54aed557297d0e94c1f2b3e4cdb35e3d
Bablu <babalu.prajapat-ext@jci.com>
2016-07-05 19:12:55 +0530

    Bablu

---
+++ src/main/java/com/jci/config/Application.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.jci.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * The Main Spring Boot Application class.<br>
+ * <br>
+ * 
+ * The {@link EnableConfigServer} annotation defines that this application will
+ * serve as the REST based API for providing external configuration. <br>
+ * <br>
+ * 
+ * The external repository from where the configuration will be picked up is
+ * 
+ * @author jci
+ */
+
+
+
+
+@EnableAutoConfiguration
+@EnableConfigServer
+@ComponentScan
+public class Application {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
